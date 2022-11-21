2512b1c345891e89c8ae05e0ec280d3acb25d212
qwsq4 <qwsq1437@gmail.com>
2022-11-21 17:55:08 +0800

    Initial commit

---
+++ src/test/java/com/example/marketspring/MarketSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.marketspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
